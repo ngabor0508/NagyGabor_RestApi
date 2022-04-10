747824b3b35dc6ac6b6902d252c1b7deddfa775c
Nagy Gabor <ngabor0508@gmail.com>
2022-04-10 22:20:56 +0200

    MainActivity osztály kész.

--- app/src/main/java/hu/dolgozat/nagygabor_restapi/MainActivity.java
+++ app/src/main/java/hu/dolgozat/nagygabor_restapi/MainActivity.java
@@ -2,17 +2,40 @@ package hu.dolgozat.nagygabor_restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button add_btn, list_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init();
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        list_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListResultActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void init(){
-
+        add_btn = findViewById(R.id.add_btn);
+        list_btn = findViewById(R.id.list_btn);
     }
 }
